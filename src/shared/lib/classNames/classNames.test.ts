554c07c7a19b394f27f31a84fd5a66f56d4e6aed
Serafima <vspish@list.ru>
2023-07-04 20:26:25 +0300

    precommit

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,4 +1,4 @@
-import {classNames} from "./classNames";
+import { classNames } from './classNames';
 
 describe('classNames', () => {
     test('with only first param', () => {
@@ -6,22 +6,22 @@ describe('classNames', () => {
     });
 
     test('with additional class', () => {
-        const expected = 'someClass class1 class2'
+        const expected = 'someClass class1 class2';
         expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
     });
 
     test('with mods', () => {
-        const expected = 'someClass mod'
-        expect(classNames('someClass', {mod: true}, )).toBe(expected);
+        const expected = 'someClass mod';
+        expect(classNames('someClass', { mod: true })).toBe(expected);
     });
 
     test('with mods false', () => {
-        const expected = 'someClass'
-        expect(classNames('someClass', {mod: false}, )).toBe(expected);
+        const expected = 'someClass';
+        expect(classNames('someClass', { mod: false })).toBe(expected);
     });
 
     test('with mods undefined', () => {
-        const expected = 'someClass'
-        expect(classNames('someClass', {mod: undefined}, )).toBe(expected);
+        const expected = 'someClass';
+        expect(classNames('someClass', { mod: undefined })).toBe(expected);
     });
-})
+});
